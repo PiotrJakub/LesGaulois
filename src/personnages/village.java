c2d1b3d34bcb7d7c19ed5ce8e349481f3764ac89
PiotrJakub <101003248+PiotrJakub@users.noreply.github.com>
2023-09-29 09:40:47 +0200

    TP2

--- src/personnages/village.java
+++ src/personnages/village.java
@@ -3,13 +3,26 @@ package personnages;
 public class Village {
 	private String nom;
 	private Chef chef;
+	private Gaulois[] villageois;
+	private int nbVillageois;
 
-	public Village(String nom) {
+	public Village(String nom, int nbVillageoisMaximum) {
 		this.nom = nom;
+		villageois = new Gaulois[nbVillageoisMaximum];
+		nbVillageois = 0;
+
 	}
 
 	public void setChef(Chef chef) {
 		this.chef = chef;
+	}
+
+	public void ajouterHabitant(Gaulois gaulois) {
+		villageois[nbVillageois] = gaulois;
+		nbVillageois++;
+	}
+	public Gaulois trouverHabitant(int numeroVillageois) {
+		return villageois[numeroVillageois];
 	}
 
 	public String getNom() {
